6fc535ced44006e1daa87c9012f3642c07d2581e
Jennilee Messenger <jennilee.messenger@gmail.com>
2023-03-27 18:17:01 -0400

    updating route files, models and seed data

---
+++ models/Thought.js
@@ -0,0 +1,28 @@
+const { Schema, Types } = require('mongoose');
+
+const thoughSchema = new Schema(
+    {
+        
+        thoughtBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+module.exports = thoughtSchema;
